8b53a7504be33045b4dc19e72e9298dcbe554406
codeasarjun <93047517+codeasarjun@users.noreply.github.com>
2022-07-31 23:40:31 +0530

    Play with dictionary

---
+++ dict.py
@@ -0,0 +1,23 @@
+# simple dictionary 
+
+
+data={
+    "Name":"A",
+    "city":"Delhi",
+    "session":2021
+
+}
+
+print(data)
+
+d=3.443443
+print(f"{d:.2f}")
+
+for key in data.keys():
+    print(key,data[key])
+
+dict={}
+name=['B','C','D','a','b']
+for i in range(5):
+    print(dict)
+print(dict)
