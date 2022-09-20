1638ef692680f6ae3b56d5c586854b1c370b9b40
codeasarjun <93047517+codeasarjun@users.noreply.github.com>
2022-09-20 12:44:53 +0530

    Right_Triangle_ using alpha

---
+++ patterns/Right_Triangle_ alpha.py
@@ -0,0 +1,16 @@
+# Right_Triangle_ using alpha
+
+'''
+A
+BC
+DEF
+GHIJ
+'''
+
+n=int(input("Please enter an interger "))
+al=0
+for i in range(1,n):
+    for j in range(1,i+1):
+        print(chr(65+al),end="")
+        al+=1
+    print('')
