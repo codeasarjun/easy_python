7caa63e8d8dc9e75890d8da472682581c50a2d00
codeasarjun <93047517+codeasarjun@users.noreply.github.com>
2022-09-24 23:55:05 +0530

    extend(),  method is used to add multiple elements at the same time at the end of the list.

---
+++ list/extend_list.py
@@ -0,0 +1,15 @@
+# extend(), this method is used to add multiple elements at the same time at the end of the list.
+
+
+'''
+Time Complexity: O(n)
+
+Space Complexity: O(1)
+'''
+
+l=[1,2,3]
+l.extend([30,50,60]) # extendig the old list 
+# make sure pass the elements in [.........] 
+print(l)
+
+
