5ea637ece8e76728c7020a5c0ef4fe8272179470
codeasarjun <93047517+codeasarjun@users.noreply.github.com>
2022-09-24 23:50:32 +0530

    append_method for adding elements in the last of the list

---
+++ list/append_the_list.py
@@ -0,0 +1,18 @@
+# append() help us to add a singel  item in the end of the list 
+# however we can add a list, tuple containg multi items 
+'''
+Time Complexity: O(1)
+
+Space Complexity: O(1)
+
+'''
+
+
+
+l=[1,2,3]
+l.append(200)# adding a single item 
+l.append((2,3,4)) # adding tuple in the list
+l.append(['a','b','v']) # adding list 
+
+print(l)
+
