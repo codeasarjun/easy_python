0ad55d931f23c10f45863ed3e01d0c46f3f6e7fb
codeasarjun <93047517+codeasarjun@users.noreply.github.com>
2022-09-24 23:52:58 +0530

    insert method help us to add element at some specific position which is not possible with append

---
+++ list/insert_element_.py
@@ -0,0 +1,13 @@
+# insert method help us to add element at some specific position which is not possible with append
+
+'''
+Time Complexity: O(n)
+
+Space Complexity: O(1)
+
+'''
+
+l=[1,2,3]
+l.insert(0,'raj')  # here I am adding raj @ 0th index
+
+print(l)
