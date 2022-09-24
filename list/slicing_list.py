2d11b58b5f02cf1dfa1ab13c84ae6db848518e55
codeasarjun <93047517+codeasarjun@users.noreply.github.com>
2022-09-25 00:14:55 +0530

    slicing process of dividing the list in some specific range

---
+++ list/slicing_list.py
@@ -0,0 +1,10 @@
+# process of dividing the list in some specfic range 
+
+l=[1,2,3,2,3]
+
+print(l[1:]) # index 1 incuded till last element 
+print(l[:4]) #from 0 index will print the value till (4-1) index
+
+print(l[::-1]) # will print the list in reversed order 
+print(l[:2:]) # will print element with increase in last by 2
+print(l)
