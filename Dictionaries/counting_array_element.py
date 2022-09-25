1f3eebd97cd9c1e92d31e2086b60a97a6a844ad5
codeasarjun <93047517+codeasarjun@users.noreply.github.com>
2022-09-25 09:52:34 +0530

    counting array element using dic

---
+++ Dictionaries/counting_array_element.py
@@ -0,0 +1,29 @@
+'''
+Time Complexity : O(n) 
+Auxiliary Space : O(n)
+
+'''
+
+
+
+from collections import defaultdict
+arr = [10, 20, 20, 10, 10, 20, 5, 20 ]
+d=dict()
+for i in range(len(arr)):
+    if arr[i] in d.keys():
+        d[arr[i]]+=1
+    else:
+        d[arr[i]]=1
+print(d)
+
+
+
+# using of defaultdict will not give key error 
+m=defaultdict(int)
+
+for i in arr:
+   m[i]+=1
+print(m)
+
+for key,value in m.items():
+    print(key,'-------',value)
