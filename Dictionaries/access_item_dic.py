4404628c0ac39531959c0951e1c987d7e97396a3
codeasarjun <93047517+codeasarjun@users.noreply.github.com>
2022-09-25 09:03:24 +0530

    accessing dictionary items

---
+++ Dictionaries/access_item_dic.py
@@ -0,0 +1,25 @@
+d={
+    'name':'raj',
+    'class':2020,
+    'place':'Earth',
+    'code in':'python'
+    
+}
+
+print(d['name'])  # by using key 
+
+print(d.get('name')) # using get function to call key value 
+
+print(d.keys()) # print all the keys 
+
+print(d.values()) # print values of the dict
+
+print(d.items())  #print key value pairs in tuple
+####################
+#####################
+#####################
+
+
+
+for key,item in d.items(): # this will unpack the dic
+    print(key,"----",item)
