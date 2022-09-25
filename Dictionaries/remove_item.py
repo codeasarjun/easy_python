1cece109ba903d4f3490331eafaf53d03a150e59
codeasarjun <93047517+codeasarjun@users.noreply.github.com>
2022-09-25 09:11:36 +0530

    removing an item from dictionary

---
+++ Dictionaries/remove_item.py
@@ -0,0 +1,11 @@
+d={
+    'name':'raj',
+    'class':2020,
+    'place':'Earth',
+    'code in':'python'
+    
+}
+d.pop('name') # remove key value 
+del d['place'] # remove mentioned key
+d.popitem() # remove last inserted item
+print(d)
